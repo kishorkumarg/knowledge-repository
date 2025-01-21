3e05647bb1d07110feba7eaa4bdd8225b75b0bec
kishorkumarg <kishorkumar.g@youngsoft.in>
2025-01-21 18:03:21 +0530

    Initial commit

---
+++ src/main/java/com/youngsoft/knowledgerepo/KnowledgeRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.youngsoft.knowledgerepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeRepositoryApplication.class, args);
+	}
+
+}
